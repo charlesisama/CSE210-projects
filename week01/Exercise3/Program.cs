5854941f962d12cf626744bd4e8897703bc97dfb
charlesisama <cisama@byupathway.edu>
2026-01-09 15:18:43 +0100

    refactor: restructure number guessing game to allow replaying

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,32 +7,41 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project: My Number Game.");
         Random randomGen = new Random();
-        int magicNumber = randomGen.Next(1, 101);
+        
+        string keepPlaying = "yes";
 
-        
-        int guessInt = -1;
-         
-        
-        while (guessInt != magicNumber)
+        while (keepPlaying == "yes")
         {
-            Console.Write("What is the guess.");
-            guessInt = int.Parse(Console.ReadLine());
+            int magicNumber = randomGen.Next(1, 101);
+            int guessInt = -1;
+            int guessCount = 0;
+            
+            while (guessInt != magicNumber)
+            {
+                Console.Write("What is the guess.");
+                guessInt = int.Parse(Console.ReadLine());
+                guessCount++;
 
-            if ( magicNumber < guessInt )
-            {
-                Console.WriteLine("Go lower");
+                if ( magicNumber < guessInt )
+                {
+                    Console.WriteLine("Go lower");
+                }
+                else if (magicNumber > guessInt)
+                {
+                    Console.WriteLine("Go higher");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations you guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses. ");
+                }  
             }
-            else if (magicNumber > guessInt)
-            {
-                Console.WriteLine("Go higher");
-            }
-            else
-            {
-                Console.WriteLine("Congratulations you guessed it!");
-            }
-               
+
+            Console.WriteLine("Do you want to play again? (yes or no)");
+            keepPlaying = Console.ReadLine();
+
         }
-
+        Console.WriteLine("Thanks for playing");        
 
     }
 }
