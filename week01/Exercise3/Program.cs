85245f4135c9345f1ac98e7f6a35e4b96492bede
charlesisama <cisama@byupathway.edu>
2026-01-09 14:39:25 +0100

    update on exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World! This is the Exercise3 Project: My Number Game.");
+        Random randomGen = new Random();
+        int magicNumber = randomGen.Next(1, 101);
+
+        
+        int guessInt = -1;
+         
+        
+        while (guessInt != magicNumber)
+        {
+            Console.Write("What is the guess.");
+            guessInt = int.Parse(Console.ReadLine());
+
+            if ( magicNumber < guessInt )
+            {
+                Console.WriteLine("Go lower");
+            }
+            else if (magicNumber > guessInt)
+            {
+                Console.WriteLine("Go higher");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations you guessed it!");
+            }
+               
+        }
+
+
     }
 }
