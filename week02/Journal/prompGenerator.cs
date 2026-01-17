e0b6373025c95033903ca88f86b72a88b9998e33
charlesisama <cisama@byupathway.edu>
2026-01-17 21:51:31 +0100

    updating more prompts to the program

--- week02/Journal/prompGenerator.cs
+++ week02/Journal/prompGenerator.cs
@@ -6,7 +6,14 @@ public class PromptGenerator
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "What is one small thing that went well today?",
+        "What made me smile today?",
+        "What is one thing I'm grateful for right now?",
+        "What scripture or thoughts stayed with me?",
+        "What did I accomplish today(big or small)?",
+        "What would I tell my future self about today?",
+        "Describe today in three words"
     };
 
     private Random _random = new Random();
