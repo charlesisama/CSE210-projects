f609cef9a5dd42751408a8530c1b0a7b7ac9005b
charlesisama <cisama@byupathway.edu>
2026-01-21 10:59:24 +0100

    abstract design for Scirpture Memorizer Program

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,24 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+    }
+
+    public void Hide()
+    {
+
+    }
+    public void Show()
+    {
+        
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
