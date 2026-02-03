cc8c1627ef19965e36888c46210455ff6654087f
charlesisama <cisama@byupathway.edu>
2026-02-03 14:38:52 +0100

    creating the base, mathAssignment and Writing Assignment classes for week05 activity

---
+++ week05/Homework/WrittingAssignment.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+public class WrittingAssignment : Assignment 
+{
+    private string _title;
+
+    public WrittingAssignment (string studentName, string topic, string title) : base(studentName, topic)
+    {
+        
+    }
+    {
+        
+    }
+}
