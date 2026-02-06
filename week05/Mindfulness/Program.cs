9e0b43c36a6741c155b17fc9fa69b86f104e5520
charlesisama <cisama@byupathway.edu>
2026-02-06 12:40:31 +0100

    welcome page for mindfulness program main class.

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+
+
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+
+        string choice = Console.ReadLine();
+
+        while (choice != "4")
+        {
+            if (choice == 1)
+            {
+                
+            }
+            if (choice == 2)
+            {
+                
+            }
+            if (choice == 3)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using the mindfulness program");
+            }
+        }
+
     }
 }
