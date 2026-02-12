fdc9fca078f1e3eea6dffe0a4b63b079ae0b6819
charlesisama <cisama@byupathway.edu>
2026-02-12 16:02:57 +0100

    week06 activity: creating foreach loop for list of shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -10,25 +10,36 @@ class Program
         string sqrColor = sqr.GetColor();
         double sqrArea = sqr.GetArea();
 
-        Console.WriteLine($"the color of the page is : {sqrColor} Area: {sqrArea}");
+        Console.WriteLine($"the color of the paper is : {sqrColor} Area: {sqrArea}");
         Console.WriteLine("");
 
         RectangleShape rct = new RectangleShape("Yellow", 5.0, 3.5);
         string rctColor = rct.GetColor();
         double rctArea = rct.GetArea();
 
-        Console.WriteLine($"the color of the page is : {rctColor} Area: {rctArea}");
+        Console.WriteLine($"the color of the paper is : {rctColor} Area: {rctArea}");
         Console.WriteLine("");
 
         CircleShape cir = new CircleShape("Green", 2.5);
         string cirColor = cir.GetColor();
         double cirArea = cir.GetArea();
 
-        Console.WriteLine($"the color of the page is : {cirColor} Area: {cirArea}");
+        Console.WriteLine($"the color of the paper is : {cirColor} Area: {cirArea}");
         Console.WriteLine("");
+        Console.WriteLine("==========================================");
 
 
         List<Shapes> shapeLists = new List<Shapes>();
+        shapeLists.Add(sqr);
+        shapeLists.Add(rct);
+        shapeLists.Add(cir);
+
+        foreach (Shapes shps in shapeLists)
+        {
+            Console.WriteLine($"The color of the paper is : {shps.GetColor()} Area: {shps.GetArea()}"); 
+        }
+
+        Console.WriteLine("==========================================");
 
 
 
